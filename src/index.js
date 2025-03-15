82f099034f9e49f21cf9a6a5471047259bc0051a
Frankincense <frankincensewelsey@gmail.com>
2025-03-15 14:08:30 +0300

    Passed deliverable test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('form');
+  form.addEventListener("submit", function(e){
+    e.preventDefault();
+    const formInput = document.querySelector("#new-task-description")
+    buildList(formInput.value)
+    form.reset()
+  })
 });
+
+function buildList(listText){
+  let li = document.createElement('li');
+  let parent = document.querySelector('#tasks')
+  li.textContent = listText;
+  parent.appendChild(li)
+  console.log(li, parent);
+} 
